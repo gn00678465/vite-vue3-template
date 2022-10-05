e7a60ade45c34936457bcbbf55ee3ea0aa1a16f1
Madao <gn00678465@gmail.com>
2022-10-05 12:04:55 +0800

    style: 修改 pinia 載入方式

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
 import { createApp } from 'vue';
-import { createPinia } from 'pinia';
+
 import router from './router/index';
+import { setupStore } from './stores';
 import './style/index.css';
-import './style.css';
+// import './style.css';
 import App from './App.vue';
 
-const pinia = createPinia();
 const app = createApp(App);
 
-app.use(pinia);
+setupStore(app);
+
 app.use(router);
 app.mount('#app');
