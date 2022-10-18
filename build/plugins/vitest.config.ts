b0bfb8b1143f94ab2c10dcaaf8037eb314080ecc
Madao <gn00678465@gmail.com>
2022-10-18 12:14:54 +0800

    build: Modify vitest config

--- build/plugins/vitest.config.ts
+++ build/plugins/vitest.config.ts
@@ -2,6 +2,7 @@
 export default {
   test: {
     globals: true,
+    include: ['test/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
     environment: 'happy-dom',
     transformMode: {
       web: [/\.[jt]sx$/]
