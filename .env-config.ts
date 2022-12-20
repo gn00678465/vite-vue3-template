9af4057610a93e0aba8d4ae3511ab4575e3c342f
Madao <gn00678465@gmail.com>
2022-12-20 10:47:56 +0800

    feat: Update environment

--- .env-config.ts
+++ .env-config.ts
@@ -7,8 +7,8 @@ const serviceEnv = {
     url: 'http://localhost:8080',
     urlPattern: '/api'
   }
-}
+};
 
 export function getServiceEnvConfig(mode: string) {
-  return serviceEnv[mode]
+  return serviceEnv[mode];
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,7 @@ module.exports = {
     extend: {
       boxShadow: {
         sider: '0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23)',
-        tab: '0 1px 2px rgb(0 21 41 / 8%)'
+        tab: '0 1px 2px rgba(0,21,41,0.08)'
       },
       colors: {
         windows: '#0178d4'
